967c99bb5a719736404ace9b1f833fc8e85e80a4
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2026-02-17 19:35:00 -0600

    fix workflow - action - 5

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 readonly env_file=${1:-".env.dev"}
+docker volume create xolo-db > /dev/null 2>&1 || true
 docker compose -p jub --env-file "$env_file" -f docker-compose.yml down
 docker compose -p jub --env-file "$env_file" -f docker-compose.yml up -d --build
 
