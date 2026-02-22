0ea04d674194057d419fac7bc16c19e0202a026d
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2026-02-22 09:47:39 -0600

    Update IaC - env files

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 readonly env_file=${1:-".env.dev"}
 docker volume create xolo-db > /dev/null 2>&1 || true
+docker network create jub > /dev/null 2>&1 || true
+
+echo "Using environment file: $env_file"
 docker compose -p jub --env-file "$env_file" -f docker-compose.yml down
 docker compose -p jub --env-file "$env_file" -f docker-compose.yml up -d --build
 
