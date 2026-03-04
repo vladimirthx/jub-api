c99ae4b0e82d1bc89dcc4b784c9e8dc6e367527d
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2026-03-04 16:52:42 -0600

    fix all conflicts

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
-import asyncio
 import pytest
 
-from jubapi.db import connect_to_mongo,close_mongo_connection
 from dotenv import load_dotenv
 import os
 
 JUB_ENV_FILE_PATH = os.environ.get("JUB_ENV_FILE_PATH", ".env.test")
+os.environ.setdefault("JUB_ENV_FILE_PATH", JUB_ENV_FILE_PATH)
 env_exists        = os.path.exists(JUB_ENV_FILE_PATH)
 
 print(f"Loading environment variables from: {JUB_ENV_FILE_PATH} - Exists: {env_exists}")
@@ -15,12 +14,14 @@ if env_exists:
 
 
 async def connect_to_database():
+    from jubapi.db import connect_to_mongo
     print("Connecting to the database...")
     await connect_to_mongo()
     # await asyncio.sleep(0.1)  # simulate async connection
 
 @pytest.fixture( autouse=True)
 async def before_all():
+    from jubapi.db import close_mongo_connection
     await connect_to_database()
     print("Database connected before tests")
     yield 
