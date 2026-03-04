1f3aff198786f845bbaa5581094a99504dafffa8
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2026-03-04 12:54:07 -0600

    fix some details about run local script

--- run_local.sh
+++ run_local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker compose -f docker-compose.yml down jub-db || true
 docker compose -f xolo.yml down || true
-docker compose -f docker-compose.yml up jub-db -d
-docker compose -f xolo.yml up -d
+docker compose -f docker-compose.yml -p jub up jub-db -d
+docker compose -f xolo.yml -p xolo up -d
 uvicorn jubapi.server:app --host ${JUB_HOST:-0.0.0.0} --port ${JUB_PORT:-5000} --reload
