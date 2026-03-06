234ceb53213714195b6e989a898397c8b0599bc0
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2026-03-06 09:33:39 -0600

    add a new parameter to decide if perform building or not

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 readonly env_file=${1:-".env.dev"}
+readonly build=${2:-1}
 docker volume create xolo-db > /dev/null 2>&1 || true
 docker network create jub > /dev/null 2>&1 || true
 
 echo "Using environment file: $env_file"
 docker compose -p jub --env-file "$env_file" -f docker-compose.yml down
-docker compose -p jub --env-file "$env_file" -f docker-compose.yml up -d --build
+
+if [ "$build" -eq 1 ]; then
+    echo "Building and starting containers..."
+    docker compose -p jub --env-file "$env_file" -f docker-compose.yml up -d --build
+else
+    echo "Starting containers without building..."
+    docker compose -p jub --env-file "$env_file" -f docker-compose.yml up -d
+fi
 
 docker compose -p jub --env-file "$env_file" -f xolo.yml down
 docker compose -p jub --env-file "$env_file" -f xolo.yml up -d
